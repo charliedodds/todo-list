a4d4a7b813ed28f239e11b5ee85adc7d34c32419
charliedodds <charlesgdodds@hotmail.co.uk>
2021-01-28 11:56:37 +0000

    Refactor todo creator functions

--- src/index.js
+++ src/index.js
@@ -28,13 +28,62 @@ instructionsProject.todos.push(
 
 projects.push(instructionsProject);
 
-console.log(projects);
-
 const createProjectCardTitle = (projectObj, parentProject) => {
   const projectTitle = document.createElement('h3');
   projectTitle.classList.add('project-title');
   projectTitle.textContent = projectObj.title;
   parentProject.appendChild(projectTitle);
+};
+
+const createEditBtn = (parentElement) => {
+  const editBtn = document.createElement('button');
+  editBtn.classList.add('todo-edit-btn');
+  const faEdit = document.createElement('i');
+  faEdit.classList.add('fas');
+  faEdit.classList.add('fa-pen');
+  editBtn.appendChild(faEdit);
+  parentElement.appendChild(editBtn);
+};
+
+const createDeleteBtn = (parentElement) => {
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('todo-delete-btn');
+  const faDelete = document.createElement('i');
+  faDelete.classList.add('fas');
+  faDelete.classList.add('fa-trash');
+  deleteBtn.appendChild(faDelete);
+  parentElement.appendChild(deleteBtn);
+};
+
+const createUtilBtns = (parentElement) => {
+  const utilContainer = document.createElement('div');
+  utilContainer.classList.add('todo-item-utils');
+  createEditBtn(utilContainer);
+  createDeleteBtn(utilContainer);
+  parentElement.appendChild(utilContainer);
+};
+
+const createCheckbox = (parentElement) => {
+  const todoCheck = document.createElement('input');
+  todoCheck.type = 'checkbox';
+  todoCheck.classList.add('todo-check');
+  parentElement.appendChild(todoCheck);
+};
+
+const createTodoTask = (todo, parentElement) => {
+  const todoItemTask = document.createElement('h3');
+  todoItemTask.classList.add('todo-item-task');
+  todoItemTask.textContent = todo.title;
+  parentElement.appendChild(todoItemTask);
+};
+
+const createTodoContainer = (todo, parentElement) => {
+  const todoItemContainer = document.createElement('div');
+  todoItemContainer.classList.add('todo-item-container');
+  createCheckbox(todoItemContainer);
+  createTodoTask(todo, todoItemContainer);
+  createUtilBtns(todoItemContainer);
+  parentElement.appendChild(todoItemContainer);
 };
 
 const createProjectTodos = (projectTodos, parentProject) => {
@@ -43,35 +92,7 @@ const createProjectTodos = (projectTodos, parentProject) => {
   for (let i = 0; i < projectTodos.length; i++) {
     const todoItem = document.createElement('li');
     todoItem.classList.add('todo-item');
-    const todoItemContainer = document.createElement('div');
-    todoItemContainer.classList.add('todo-item-container');
-    const todoCheck = document.createElement('input');
-    todoCheck.type = 'checkbox';
-    todoCheck.classList.add('todo-check');
-    todoItemContainer.appendChild(todoCheck);
-    const todoItemTask = document.createElement('div');
-    todoItemTask.classList.add('todo-item-task');
-    todoItemTask.textContent = projectTodos[i].title;
-    todoItemContainer.appendChild(todoItemTask);
-    const utilContainer = document.createElement('div');
-    utilContainer.classList.add('todo-item-utils');
-    const editBtn = document.createElement('button');
-    editBtn.classList.add('todo-edit-btn');
-    const faEdit = document.createElement('i');
-    faEdit.classList.add('fas');
-    faEdit.classList.add('fa-pen');
-    editBtn.appendChild(faEdit);
-    utilContainer.appendChild(editBtn);
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('todo-delete-btn');
-    const faDelete = document.createElement('i');
-    faDelete.classList.add('fas');
-    faDelete.classList.add('fa-trash');
-    deleteBtn.appendChild(faDelete);
-    utilContainer.appendChild(deleteBtn);
-
-    todoItemContainer.appendChild(utilContainer);
-    todoItem.appendChild(todoItemContainer);
+    createTodoContainer(projectTodos[i], todoList);
     todoList.appendChild(todoItem);
   }
   parentProject.appendChild(todoList);
@@ -80,35 +101,45 @@ const createProjectTodos = (projectTodos, parentProject) => {
 const handleProjectBtnClick = (e) => {
   const parent = e.target.closest('.project-card');
   parent.classList.toggle('hide-todo-list');
-  e.target.classList.toggle('flip-down');
-  e.target.classList.toggle('flip-up');
+  e.target.classList.toggle('flip');
 };
 
-const createProjectCardFooter = (projectObj, parentProject) => {
-  const projectFooter = document.createElement('footer');
-  projectFooter.classList.add('project-footer');
+const createFooterNums = (projectObj, parentElement) => {
   const numTodos = document.createElement('p');
   numTodos.classList.add('num-todos');
   numTodos.textContent = `${projectObj.todos.length} todo${
     projectObj.todos.length === 1 ? '' : 's'
   }`;
-  projectFooter.appendChild(numTodos);
+  parentElement.appendChild(numTodos);
+};
+
+const createAddTodoBtn = (parentElement) => {
   const addTodoBtn = document.createElement('button');
   addTodoBtn.classList.add('add-todo-btn');
   const faPlus = document.createElement('i');
   faPlus.classList.add('fas');
   faPlus.classList.add('fa-plus');
   addTodoBtn.appendChild(faPlus);
-  projectFooter.appendChild(addTodoBtn);
+  parentElement.appendChild(addTodoBtn);
+};
+
+const createProjectToggler = (parentElement) => {
   const projectBtn = document.createElement('button');
   projectBtn.classList.add('project-button');
   const faChevron = document.createElement('i');
   faChevron.classList.add('fas');
-  faChevron.classList.add('fa-chevron-up');
-  faChevron.classList.add('flip-down');
+  faChevron.classList.add('fa-chevron-down');
   projectBtn.appendChild(faChevron);
   projectBtn.addEventListener('click', handleProjectBtnClick);
-  projectFooter.appendChild(projectBtn);
+  parentElement.appendChild(projectBtn);
+};
+
+const createProjectCardFooter = (projectObj, parentProject) => {
+  const projectFooter = document.createElement('footer');
+  projectFooter.classList.add('project-footer');
+  createFooterNums(projectObj, projectFooter);
+  createAddTodoBtn(projectFooter);
+  createProjectToggler(projectFooter);
   parentProject.appendChild(projectFooter);
 };
 
@@ -123,6 +154,8 @@ const createProjectCard = (project) => {
   main.appendChild(projectCard);
   return projectCard;
 };
+
+// add new project logic
 
 const addProjectBtn = document.querySelector('.add-project-btn');
 const addProjectForm = document.querySelector('.add-project-form');
@@ -141,12 +174,12 @@ const createProject = (e) => {
   projects.push(newProject);
   createProjectCard(newProject);
   toggleAddProjectForm();
-  console.log(projects);
 };
 
 addProjectForm.addEventListener('submit', createProject);
 
+// create initial project
+
 for (let i = 0; i < projects.length; i++) {
-  console.log(projects[i]);
   createProjectCard(projects[i]);
 }
